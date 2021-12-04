b5a1f055d73d3e17b78025aefc5e5e0f41013e53
lvalencacomputacao <53199818+lvalencacomputacao@users.noreply.github.com>
2021-12-03 22:12:14 -0300

    Algoritmo de Floyd-Warshall feito

---
+++ floyd_warshall.py
@@ -0,0 +1,33 @@
+INF = 9999999999999
+
+def matriz_de_adjacencia(grafo):
+    n = grafo.qtdVertices()
+    m = grafo.qtdArestas()
+    matriz = [[INF for i in range(n)] for j in range(n)]
+
+    for edge in grafo.arestas:
+        x = edge.vertices[0]
+        y = edge.vertices[1]
+        matriz[x-1][y-1] = edge.peso
+        matriz[y-1][x-1] = edge.peso
+
+    for i in range(n):  
+        matriz[i][i] = 0
+
+    return matriz
+
+def floyd_warshall(grafo):
+    matriz_adjancencia = matriz_de_adjacencia(grafo)
+    print(matriz_adjancencia)
+    dist = list(map(lambda p: list(map(lambda q: q, p)), matriz_adjancencia))
+    n = len(matriz_adjancencia)
+
+    for r in range(n):
+        for p in range(n):
+            for q in range(n):
+                dist[p][q] = min(dist[p][q], dist[p][r] + dist[r][q])
+    for i in range(n):
+        print("{}:".format(i+1), end="")
+        for j in range(n-1):
+            print("{},".format(dist[i][j]), end="")
+        print(dist[i][n-1])
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from grafo import Grafo
 from dijkstra import Dijkstra
 from busca import Busca
+from floyd_warshall import *
 
 grafo1 = Grafo()
 grafo1.ler('fln_pequena.net')
 busca = Busca(grafo1,2)
 dijkstra = Dijkstra(grafo1,2)
+
+grafo2 = Grafo()
+grafo2.ler("teste.txt")
+floyd_warshall(grafo2)
+print("\n")
+floyd_warshall(grafo1)
+
+# Testar para casos maiores depois 
